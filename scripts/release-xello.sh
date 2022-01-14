2988a4a933323da22cb56005f1347f1134fba90b
EduardoSantanaSeverino <eduardoamparos@gmail.com>
2022-01-14 18:57:32 -0500

    fix version

--- scripts/release-xello.sh
+++ scripts/release-xello.sh
@@ -1,3 +1,3 @@
-./scripts/release-dockerhub.sh -s dashboard -t v3
-./scripts/release-dockerhub.sh -s director -t v3
-./scripts/release-dockerhub.sh -s api -t v3
+./scripts/release-dockerhub.sh -s dashboard -t v4
+./scripts/release-dockerhub.sh -s director -t v4
+./scripts/release-dockerhub.sh -s api -t v4
